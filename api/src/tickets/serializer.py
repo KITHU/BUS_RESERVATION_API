6e03af5e87bc6b51372a9495f444e14d4cf60ba0
KITHU <james.kithu2@gmail.com>
2022-02-14 11:07:20 +0300

    travel date cannot be in the past

--- api/src/tickets/serializer.py
+++ api/src/tickets/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models.models import Bus, Schedule, Passager, Reservation, Route
+from datetime import *
 
 
 class DynamicDepthSerializer(serializers.ModelSerializer):
@@ -34,10 +35,9 @@ class ScheduleSerializer(serializers.ModelSerializer):
     available_seats=serializers.SerializerMethodField(read_only=True)
 
     def get_available_seats(self,obj):
-        t= obj.bus_id.no_of_seat
+        t= obj.bus.no_of_seat 
         total = set(range(1,t+1))
         booked = Reservation.objects.values_list('seat_no',flat=True).filter(schedule=obj.id)
-        # import pdb; pdb.set_trace()
         return total.difference(set(booked))
         
     def __init__(self, *args, **kwargs):
@@ -45,19 +45,20 @@ class ScheduleSerializer(serializers.ModelSerializer):
         try:
             if self.context['request'].method in ['GET', 'RETRIEVE', ]:
                 self.fields['route'] = RouteSerializer()
-                self.fields['bus_id'] = BusSerializer()
+                self.fields['bus'] = BusSerializer()
         except KeyError:
             pass
         
     def create(self, validated_data):
-
+        travel_date = validated_data.get('date_of_travel')
+        today_date = date.today()
+        if today_date > travel_date:
+            raise serializers.ValidationError("Travel date cannot be in the past")
         return super().create(validated_data)  
 
     class Meta:
         model = Schedule
         fields = '__all__'
-
-
 
 
 class ReservationSerializer(serializers.ModelSerializer):
